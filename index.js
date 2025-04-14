400275f95e6c487d147a1c90e2679e1b7c31eeb0
Emmah Gicheru <gicheruemmah@gmail.com>
2025-04-15 00:33:46 +0300

    Intial Commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Employee details
+const employee = {
+  name: "Llewellyn Muirwe",
+  streetAddress: "Heaven Street",
+};
+
+// 1. updateEmployeeWithKeyAndValue - Non-destructive update
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+// 2. destructivelyUpdateEmployeeWithKeyAndValue - Destructive update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// 3. deleteFromEmployeeByKey - Non-destructive delete
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// 4. destructivelyDeleteFromEmployeeByKey - Destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
